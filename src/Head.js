0f4ac2b1d230122dc6ae3bb64c37a2966297669d
rapidmon <ehhwll@hanmail.net>
2021-11-10 01:52:19 +0900

    1차 완료

--- src/Head.js
+++ src/Head.js
@@ -1,11 +1,21 @@
 import style from './style'
+import Weather from './Weather'
+import React from 'react';
+import {useTodoState} from './Todocontext';
 
-function Head({children}){
+function TodoHead({children}){
+    const todos = useTodoState();
+    const undoneTasks = todos.filter(todo => !todo.done);
+    const date = new Date();
+    const dayname = date.toLocaleDateString('Ko-KR', { weekday: 'long' });
+
+    console.log(date);
     return <style.headblock>
-        <h1>2019년 7월 10일</h1>
-        <div className="day">수요일</div>
-        <div className="tasks-left">할 일 2개 남음</div>
+        <h1>{date.getFullYear()}년 {date.getMonth()+1}월 {date.getDate()}일</h1>
+        <div className="weather"><Weather /></div>
+        <div className="day">{dayname}</div>
+        <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </style.headblock>
   }
   
-export default Head;
+export default TodoHead;
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import axios from 'axios'
+import React, {useState} from 'react';
+import style from './style';
+
+// 버튼 css
+const DivButton = styled.div`
+  flex: 10;
+  position: relative;
+  width: 110px;
+  height: 15px;
+  float: left;
+  border-radius: 50%;
+  padding: 5px;
+  margin-bottom: 20px;
+  line-height: 12px;
+  padding-left: 0px;
+  cursor: pointer;
+  &: hover {
+    background-color: white;
+    color: #38d9a9;
+  }
+`
+
+// api를 불러오는 방식 코드
+function Weather(){
+    const [weather, setWeather] = useState('');
+    const clickHandler = async() =>{
+        const API_KEY = 'd4389cad7412a6a110847e67b352fffb';
+        const CITY_NAME = 'SEOUL';
+        await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
+        ).then((response) => {
+        setWeather(response.data.weather[0].main);
+        })
+    };
+    return(
+    <div>
+      <DivButton onClick={clickHandler}>Today Weather </DivButton><style.weather>{weather}</style.weather>
+    </div>
+    )
+}
+
+export default Weather
